308a2de991250396102db48ddaa592af890e82d5
Yohann Dumortier <yohann.dumortier@gmail.com>
2018-04-17 14:48:34 +0200

    modification de graph.php pour afficher un graph

--- page/graph.php
+++ page/graph.php
@@ -11,32 +11,23 @@ try{
 }
 
 // appel des bibliothèque de jpgraph
+
 require_once ('../jpgraph-4.2.0/src/jpgraph.php');
 require_once ('../jpgraph-4.2.0/src/jpgraph_line.php');
 
-
-//Valeur dans le graphe
-/*
+// Some data
 $ydata = array(11,3,8,12,5,1,9,13,5,7);
-*/
 
 // Create the graph. These two calls are always required
-/*$graph = new Graph(350,250);
-$graph->SetScale('textlin');*/
+$graph = new Graph(700,500);
+$graph->SetScale('intlin');
 
 // Create the linear plot
-/*
 $lineplot=new LinePlot($ydata);
-$lineplot->SetColor('blue');
-*/
 
 // Add the plot to the graph
-/*
 $graph->Add($lineplot);
-*/
 
 // Display the graph
-/*
 $graph->Stroke();
-*/
 ?>
